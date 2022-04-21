9f127971b8dd2c008f3c25689c30c8943890c2a2
LetsWorkb1 <jhow.win@gmail.com>
2022-04-21 10:19:39 -0300

    Criação da paginação de usuários e melhorando a componentização

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,32 @@ import { AppProps } from 'next/app';
 
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme';
+
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+import { makeServer } from '../services/mirage';
+import { queryClient } from '../services/queryCliente';
+
+
+if (process.env.NODE_ENV !== 'production') {
+  makeServer();
+}
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
